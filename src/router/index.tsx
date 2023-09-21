8c2225a59f86b783688d1cbda131c60e099748b1
bimus-github <bimus2022@gmail.com>
2023-09-21 09:20:31 +0500

    router

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,9 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
-import { About } from "../pages";
+import { About, Designs, Services } from "../pages";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <About />,
+  },
+  {
+    path: "/services",
+    element: <Services />,
+  },
+  {
+    path: "/designs",
+    element: <Designs />,
   },
 ]);
