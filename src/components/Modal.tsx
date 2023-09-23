e4e9713f6e63ae6870c1d598da9383b0ee186d57
bimus-github <bimus2022@gmail.com>
2023-09-23 08:50:26 +0500

    fixing modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -9,15 +9,25 @@ interface ModalProps {
 
 function Modal(props: ModalProps) {
   const { isOpenModal, setIsOpenModal, children, handleYes } = props;
+
+  const handleBackgroundClick = (
+    event: React.MouseEvent<HTMLDialogElement>
+  ) => {
+    const target = event.target as HTMLDialogElement;
+    if (target.classList.contains("modal-bg")) {
+      setIsOpenModal(false);
+    }
+  };
   return (
     <dialog
       id="dialog"
-      className={`${styles.dialog} ${isOpenModal ? "flex" : "hidden"}`}
+      className={`modal-bg ${styles.dialog} ${isOpenModal ? "flex" : "hidden"}`}
+      onClick={handleBackgroundClick}
     >
       <main className={styles.main}>
         <h1 className={styles.h1}>Modal</h1>
         <div className={styles.div}>{children}</div>
-        <div className="flex gap-3">
+        <div className="flex gap-3 sm:flex-col w-full">
           <button
             onClick={() => {
               handleYes && handleYes();
@@ -57,7 +67,7 @@ const styles = {
   h1: "font-semibold text-[20px] sm:text-[18px] animate-spin-speed-one-time ",
   div: "w-full h-full p-2",
   btn:
-    " w-[100px] text-center font-semibold px-4 py-1 rounded-md transition ease-in-out delay-150 hover:scale-110  duration-300 drop-shadow-md",
+    " w-[100px] sm:w-full text-center font-semibold px-4 py-1 rounded-md transition ease-in-out delay-150 hover:scale-110  duration-300 drop-shadow-md",
 
   btnSave: "bg-blue-500  hover:bg-blue-300",
   btnClose: "bg-green-500 hover:bg-green-300",
