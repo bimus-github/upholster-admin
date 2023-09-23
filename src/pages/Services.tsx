05d015338d54fe1c057282692b148a7145e9ec3f
bimus-github <bimus2022@gmail.com>
2023-09-23 08:50:59 +0500

    services section

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { ServicesSection } from "../components";
 
 function Services() {
-  return <div>Services</div>;
+  return (
+    <section className={styles.main}>
+      {/* hismat turlari */}
+      <ServicesSection />
+    </section>
+  );
 }
 
 export default Services;
+
+const styles = {
+  main: "min-h-[100vh] flex flex-col p-10 items-center gap-10",
+};
