705657830f7208eba2767ce9fc3ec888a82c9910
bimus-github <bimus2022@gmail.com>
2023-09-23 09:02:08 +0500

    fixing dialog

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 
+import CheckIcon from "@mui/icons-material/Check";
+import CloseIcon from "@mui/icons-material/Close";
+
 interface DialogProps {
   isOpenDialog: boolean;
   setIsOpenDialog: React.Dispatch<React.SetStateAction<boolean>>;
@@ -10,13 +13,24 @@ interface DialogProps {
 function Dialog(props: DialogProps) {
   const { isOpenDialog, setIsOpenDialog, children, handleYes } = props;
 
+  const handleBackgroundClick = (
+    event: React.MouseEvent<HTMLDialogElement>
+  ) => {
+    const target = event.target as HTMLDialogElement;
+    if (target.classList.contains("dialog-bg")) {
+      setIsOpenDialog(false);
+    }
+  };
+
   return (
     <dialog
       id="dialog"
-      className={`${styles.dialog} ${isOpenDialog ? "flex" : "hidden"}`}
+      className={`dialog-bg ${styles.dialog} ${
+        isOpenDialog ? "flex" : "hidden"
+      }`}
+      onClick={handleBackgroundClick}
     >
       <main className={styles.main}>
-        <h1 className={styles.h1}>Are you sure?</h1>
         <div className={styles.div}>{children}</div>
         <div className="flex gap-3">
           <button
@@ -26,7 +40,7 @@ function Dialog(props: DialogProps) {
             }}
             className={styles.btn + " " + styles.btnSave}
           >
-            <span>Ha</span>
+            <CheckIcon />
           </button>
           <button
             onClick={() => {
@@ -34,7 +48,7 @@ function Dialog(props: DialogProps) {
             }}
             className={styles.btn + " " + styles.btnClose}
           >
-            Yo'q
+            <CloseIcon />
           </button>
         </div>
       </main>
@@ -46,14 +60,12 @@ export default Dialog;
 
 const styles = {
   dialog:
-    "w-full min-h-[100vh] fixed top-0 flex-col justify-center items-center z-20 bg-gray-800 bg-opacity-30",
+    "w-full min-h-[100vh] fixed top-0 flex-col justify-end items-start z-20 bg-gray-800 bg-opacity-10 p-5",
   main:
-    " animate-ping-one-time w-[400px] md:w-[350px] sm:w-[300px] min-h-[200px] bg-white drop-shadow-2xl rounded-lg p-5 flex flex-col items-center justify-around",
-  h1: "font-semibold text-[20px] sm:text-[18px] animate-spin-speed-one-time",
-  div: "w-full h-full p-2",
+    " animate-ping-one-time  bg-white drop-shadow-2xl rounded-lg p-4 flex items-center justify-around gap-2",
+  div: "w-full h-full",
   btn:
-    " w-[100px] text-center font-semibold px-4 py-1 rounded-md transition ease-in-out delay-150 hover:scale-110 duration-300",
+    "text-center font-semibold p-1 rounded-full transition ease-in-out delay-150 hover:scale-110 duration-300",
   btnSave: "bg-blue-500  hover:bg-blue-300",
   btnClose: "bg-green-500 hover:bg-green-300",
-  span: " text-white duration-100 delay-100",
 };
