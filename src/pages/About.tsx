8a7470770bf75d53901436abc2cb41f7df23ad04
bimus-github <bimus2022@gmail.com>
2023-09-25 18:18:01 +0500

    getting garage Image and tel numbers

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,10 +1,29 @@
+import { useEffect } from "react";
 import {
   GarageImageSection,
   LocationInfoSection,
   TelInfoSection,
 } from "../components";
+import { getGarageImage, getTelNumbers } from "../firebase/functions/info";
+import { useAppDispatch } from "../store/hooks";
+import { infoActions } from "../store/features/infoSlices";
 
 function About() {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    getGarageImage()
+      .then((url) => {
+        if (url) dispatch(infoActions.setGarageImage(url.url));
+      })
+      .finally(() => {});
+  }, [dispatch]);
+
+  useEffect(() => {
+    getTelNumbers().then((data) => {
+      if (data) dispatch(infoActions.setNumbers(data.number));
+    });
+  }, [dispatch]);
   return (
     <section className={styles.main}>
       {/*garage image section */}
