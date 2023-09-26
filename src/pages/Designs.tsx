118cac37ffd8a480b4c3611982b6aff1381eb6aa
bimus-github <bimus2022@gmail.com>
2023-09-26 19:14:42 +0500

    getting cars and services

--- src/pages/Designs.tsx
+++ src/pages/Designs.tsx
@@ -1,8 +1,37 @@
-import React from "react";
-import { Cars } from "../components";
+import React, { useEffect } from "react";
+import { Cars, ImageLoading } from "../components";
 import { Outlet } from "react-router-dom";
+import { getCars } from "../firebase/functions/car";
+import { useAppDispatch } from "../store/hooks";
+import { carActions } from "../store/features/carSlices";
+import { getServices } from "../firebase/functions/service";
+import { serviceAction } from "../store/features/serviceSlices";
 
 function Designs() {
+  const dispatch = useAppDispatch();
+  const [isLoading, setIsLoading] = React.useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    getCars().then((data) => {
+      data && dispatch(carActions.setCars(data));
+    });
+
+    getServices()
+      .then((data) => {
+        data && dispatch(serviceAction.setServices(data));
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, [dispatch]);
+
+  if (isLoading)
+    return (
+      <div className="h-[100vh] w-full flex justify-center items-center">
+        <ImageLoading />
+      </div>
+    );
   return (
     <section className={styles.main}>
       {/* Cars section */}
