4dd60d4079a260175c64c32a4f86eed85ba2261e
bimus-github <bimus2022@gmail.com>
2023-09-26 20:54:18 +0500

    adding and deleting car

--- src/components/designs/Cars.tsx
+++ src/components/designs/Cars.tsx
@@ -1,75 +1,203 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
 import { Tooltip } from "@mui/material";
 import { useNavigate } from "react-router-dom";
-
-const damas =
-  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ732b4s6iTlVg2XPePnCRawVLejqsiEy4Ge7HTL7hqhg&s";
+import { useAppDispatch, useAppSelector } from "../../store/hooks";
+import Modal from "../Modal";
+import Input from "../Input";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { storage } from "../../firebase";
+import { addCar, deleteCar } from "../../firebase/functions/car";
+import { carActions } from "../../store/features/carSlices";
+import Dialog from "../Dialog";
 
 function Cars() {
+  const dispatch = useAppDispatch();
+  const cars = useAppSelector((state) => state.cars);
+  const fileInputRef = useRef<HTMLInputElement>(null);
   const navigate = useNavigate();
+
+  const [isAddModalOpen, setIsAddModalOpen] = useState<boolean>(false);
+  const [isDeleteDialogOpen, setIsDeleteDialogOpen] = useState<boolean>(false);
+  const [isSaving, setIsSaving] = useState<boolean>(false);
+  const [isUploadingImage, setIsUploadingImage] = useState<boolean>(false);
+  const [isDeleting, setIsDeleting] = useState<boolean>(false);
+
+  const [carName, setCarName] = useState<string>("");
+  const [carImage, setCarImage] = useState<string>("");
+  const [deletedCar, setDeletedCar] = useState<string>("");
+
+  const handleUploadImage = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+
+    if (!file) return;
+    setIsUploadingImage(true);
+
+    const storageRef = ref(storage, "images/" + file.name);
+
+    uploadBytes(storageRef, file)
+      .then((snapshot) => {
+        console.log("Uploaded a file!");
+
+        getDownloadURL(storageRef).then((url) => {
+          setCarImage(url);
+        });
+      })
+      .finally(() => {
+        setIsUploadingImage(false);
+      });
+  };
+
+  const handleSaveCar = () => {
+    if (carName.length === 0) return;
+    if (carImage.length === 0) return;
+    setIsSaving(true);
+
+    addCar({
+      name: carName,
+      image: carImage,
+      services: [],
+    })
+      .then(() => {
+        setIsAddModalOpen(false);
+        dispatch(
+          carActions.addCar({ name: carName, image: carImage, services: [] })
+        );
+      })
+      .finally(() => {
+        setCarImage("");
+        setCarName("");
+        setIsSaving(false);
+      });
+  };
+
+  const handleDeleteCar = () => {
+    setIsDeleting(true);
+    setIsDeleteDialogOpen(false);
+
+    deleteCar(deletedCar)
+      .then(() => {
+        dispatch(carActions.deleteCar({ name: deletedCar }));
+      })
+      .finally(() => {
+        setIsDeleting(false);
+        setDeletedCar("");
+      });
+  };
+
   return (
     <fieldset className={styles.fieldset}>
       <legend className={styles.legend}>
         <cite className={styles.cite}>Mashinalar</cite>
         <Tooltip title="Qo'shish">
-          <button className={`${styles.iconBtn}  ${styles.addIcon}`}>
+          <button
+            className={`${styles.iconBtn}  ${styles.addIcon}`}
+            onClick={() => setIsAddModalOpen(true)}
+          >
             <AddCircleIcon />
           </button>
         </Tooltip>
       </legend>
 
       <ul className={styles.ul}>
-        <li className={styles.li}>
-          <div className={styles.div}>
-            <picture className={styles.picture}>
-              <img src={damas} alt="" className={styles.image} />
-            </picture>
-            <strong
-              className="text-sky-500 cursor-pointer"
-              onClick={() => navigate("/designs/1")}
-            >
-              Damas
-            </strong>
-          </div>
+        {cars.length === 0 && <p>Mashinalar mavjud emas</p>}
+        {cars.map((car, index) => (
+          <li className={styles.li} key={index}>
+            <div className={styles.div}>
+              <picture className={styles.picture}>
+                <img src={car.image} alt="car_image" className={styles.image} />
+              </picture>
+              <strong
+                className="text-sky-500 cursor-pointer"
+                onClick={() => navigate(`/designs/${car.name}`)}
+              >
+                {car.name}
+              </strong>
+            </div>
 
-          <div className={styles.div}>
-            <Tooltip title="O'zgartirish">
-              <button className={`${styles.iconBtn}  ${styles.editIcon}`}>
-                <EditIcon />
-              </button>
-            </Tooltip>
-            <Tooltip title="O'chirish">
-              <button className={`${styles.iconBtn} ${styles.deleteIcon}`}>
-                <DeleteIcon />
-              </button>
-            </Tooltip>
-          </div>
-        </li>
-        <li className={styles.li}>
-          <div className={styles.div}>
-            <picture className={styles.picture}>
-              <img src={damas} alt="" className={styles.image} />
-            </picture>
-            <strong>Damas</strong>
-          </div>
-
-          <div className={styles.div}>
-            <Tooltip title="O'zgartirish">
-              <button className={`${styles.iconBtn}  ${styles.editIcon}`}>
-                <EditIcon />
-              </button>
-            </Tooltip>
-            <Tooltip title="O'chirish">
-              <button className={`${styles.iconBtn} ${styles.deleteIcon}`}>
-                <DeleteIcon />
-              </button>
-            </Tooltip>
-          </div>
-        </li>
+            <div className={styles.div}>
+              <Tooltip title="O'zgartirish">
+                <button className={`${styles.iconBtn}  ${styles.editIcon}`}>
+                  <EditIcon />
+                </button>
+              </Tooltip>
+              {isDeleting && car.name === deletedCar ? (
+                <div className="inline-block w-8 h-8 border-t-4 border-black rounded-full" />
+              ) : (
+                <Tooltip title="O'chirish">
+                  <button
+                    className={`${styles.iconBtn} ${styles.deleteIcon}`}
+                    onClick={() => {
+                      setIsDeleteDialogOpen(true);
+                      setDeletedCar(car.name);
+                    }}
+                  >
+                    <DeleteIcon />
+                  </button>
+                </Tooltip>
+              )}
+            </div>
+          </li>
+        ))}
       </ul>
+
+      <Dialog
+        isOpenDialog={isDeleteDialogOpen}
+        setIsOpenDialog={setIsDeleteDialogOpen}
+        handleYes={handleDeleteCar}
+      >
+        Rostanham o'chitmoqchi missiz?
+      </Dialog>
+
+      <Modal
+        isSaving={isSaving}
+        isOpenModal={isAddModalOpen}
+        setIsOpenModal={setIsAddModalOpen}
+        handeClear={() => {
+          setCarImage("");
+          setCarName("");
+        }}
+        handleYes={handleSaveCar}
+      >
+        <div className={modalstyles.main}>
+          <Input
+            placeholder="m.u: Damas"
+            value={carName}
+            setValue={setCarName}
+            type="text"
+            title="Mashina nomi"
+          />
+
+          <div className={modalstyles.imageDiv}>
+            <input
+              type="file"
+              ref={fileInputRef}
+              onChange={handleUploadImage}
+              className="hidden"
+            />
+            <Tooltip title="Rasm yuklash">
+              <button
+                className={`${styles.iconBtn} ${styles.addIcon}`}
+                onClick={() => fileInputRef?.current?.click()}
+              >
+                {isUploadingImage ? (
+                  <div className="animate-spin inline-block w-8 h-8 border-t-4 border-black rounded-full" />
+                ) : (
+                  <AddCircleIcon />
+                )}
+              </button>
+            </Tooltip>
+
+            {carImage.length !== 0 && (
+              <picture className={modalstyles.picture}>
+                <img className={modalstyles.image} src={carImage} alt="" />
+              </picture>
+            )}
+          </div>
+        </div>
+      </Modal>
     </fieldset>
   );
 }
@@ -92,4 +220,13 @@ const styles = {
   addIcon: "text-sky-500",
   editIcon: "text-blue-500",
   deleteIcon: "text-red-500",
+};
+
+const modalstyles = {
+  main: "w-full h-full flex flex-col justify-center items-center gap-4",
+
+  imageDiv: "flex gap-2 items-center justify-center",
+  picture:
+    " w-[100px] h-[100px] rounded-full overflow-hidden relative flex justify-center items-center",
+  image: " absolute h-full",
 };
