9c383abf40d86e18959da7fa01e589e3098dcb6c
bimus-github <bimus2022@gmail.com>
2023-09-27 13:28:42 +0500

    working on update item

--- src/firebase/functions/car.ts
+++ src/firebase/functions/car.ts
@@ -102,64 +102,279 @@ export const addServiceToCar = async ({
   }
 };
 
-export const updateServiceByCar = async ({
-  name,
-  service,
+export const updateBeforeImage = async ({
+  carName,
+  serviseName,
+  beforeImage,
+  itemId,
 }: {
-  name: string;
-  service: Car_Service_Type;
+  carName: string;
+  serviseName: string;
+  beforeImage: string;
+  itemId: string;
 }) => {
   try {
     // getting the services from the car
-    const docRef = doc(db, collection_name, name);
+    const docRef = doc(db, collection_name, carName);
     const docSnap = await getDoc(docRef);
 
     if (docSnap.exists()) {
       const services = docSnap.data().services as Car_Service_Type[];
 
-      // updating the service from the car
-      const updatedServices = services.map((item) => {
-        if (item.name === service.name) {
-          return service;
-        } else {
-          return item;
-        }
-      });
+      // find the service in the car
+      const foundService = services.find((item) => item.name === serviseName);
 
-      await updateDoc(docRef, {
-        services: updatedServices,
-      });
+      if (foundService) {
+        // update before image of the item
+        const updatedServices = services.map(({ name, items }) => {
+          if (name === serviseName) {
+            return {
+              name,
+              items: items.map((item) => {
+                if (item.id === itemId) {
+                  return {
+                    ...item,
+                    beforeImage,
+                  };
+                } else {
+                  return item;
+                }
+              }),
+            } as Car_Service_Type;
+          } else {
+            return { name, items };
+          }
+        });
+
+        await updateDoc(docRef, {
+          services: updatedServices,
+        });
+      } else {
+        console.log("No such car!");
+      }
     }
-    console.log("Service updated to: ", name);
   } catch (error) {
-    console.log("Error while updating service to car: ", error);
+    console.log("Error while updating before image: ", error);
   }
 };
 
-export const deleteServiceFromCar = async ({
-  name,
-  serviceName,
+export const updateThenImage = async ({
+  carName,
+  serviseName,
+  thenImage,
+  itemId,
 }: {
-  name: string;
-  serviceName: string;
+  carName: string;
+  serviseName: string;
+  thenImage: string;
+  itemId: string;
 }) => {
   try {
     // getting the services from the car
-    const docRef = doc(db, collection_name, name);
+    const docRef = doc(db, collection_name, carName);
     const docSnap = await getDoc(docRef);
 
     if (docSnap.exists()) {
       const services = docSnap.data().services as Car_Service_Type[];
 
-      // removing the service from the car
-      await updateDoc(docRef, {
-        services: services.filter((item) => item.name !== serviceName),
-      });
+      // find the service in the car
+
+      const foundService = services.find((item) => item.name === serviseName);
+
+      if (foundService) {
+        // update then image of the item
+        const updatedServices = services.map(({ name, items }) => {
+          if (name === serviseName) {
+            return {
+              name,
+              items: items.map((item) => {
+                if (item.id === itemId) {
+                  return {
+                    ...item,
+                    thenImage,
+                  };
+                } else {
+                  return item;
+                }
+              }),
+            } as Car_Service_Type;
+          } else {
+            return { name, items };
+          }
+        });
+
+        await updateDoc(docRef, {
+          services: updatedServices,
+        });
+      } else {
+        console.log("No such car!");
+      }
+    }
+  } catch (error) {
+    console.log("Error while updating then image: ", error);
+  }
+};
+
+export const updateDescription = async ({
+  carName,
+  serviseName,
+  description,
+  itemId,
+}: {
+  carName: string;
+  serviseName: string;
+  description: string;
+  itemId: string;
+}) => {
+  try {
+    // getting the services from the car
+    const docRef = doc(db, collection_name, carName);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      const services = docSnap.data().services as Car_Service_Type[];
+
+      // find the service in the car
+      const foundService = services.find((item) => item.name === serviseName);
+
+      if (foundService) {
+        // update description of the item
+        const updatedServices = services.map(({ name, items }) => {
+          if (name === serviseName) {
+            return {
+              name,
+              items: items.map((item) => {
+                if (item.id === itemId) {
+                  return {
+                    ...item,
+                    description,
+                  };
+                } else {
+                  return item;
+                }
+              }),
+            } as Car_Service_Type;
+          } else {
+            return { name, items };
+          }
+        });
+
+        await updateDoc(docRef, {
+          services: updatedServices,
+        });
+      } else {
+        console.log("No such car!");
+      }
     } else {
-      // docSnap.data() will be undefined in this case
       console.log("No such car!");
     }
   } catch (error) {
-    console.log("Error while deleting service from car: ", error);
+    console.log("Error while updating description: ", error);
+  }
+};
+
+export const updatePrice = async ({
+  carName,
+  serviseName,
+  price,
+  itemId,
+}: {
+  carName: string;
+  serviseName: string;
+  price: string;
+  itemId: string;
+}) => {
+  try {
+    // getting the services from the car
+    const docRef = doc(db, collection_name, carName);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      const services = docSnap.data().services as Car_Service_Type[];
+
+      // find the service in the car
+      const foundService = services.find((item) => item.name === serviseName);
+
+      if (foundService) {
+        // update price of the item
+        const updatedServices = services.map(({ name, items }) => {
+          if (name === serviseName) {
+            return {
+              name,
+              items: items.map((item) => {
+                if (item.id === itemId) {
+                  return {
+                    ...item,
+                    price,
+                  };
+                } else {
+                  return item;
+                }
+              }),
+            } as Car_Service_Type;
+          } else {
+            return { name, items };
+          }
+        });
+
+        await updateDoc(docRef, {
+          services: updatedServices,
+        });
+      } else {
+        console.log("No such car!");
+      }
+    } else {
+      console.log("No such car!");
+    }
+  } catch (error) {
+    console.log("Error while updating price: ", error);
+  }
+};
+
+export const deleteItem = async ({
+  carName,
+  serviseName,
+  itemId,
+}: {
+  carName: string;
+  serviseName: string;
+  itemId: string;
+}) => {
+  try {
+    // getting the services from the car
+    const docRef = doc(db, collection_name, carName);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      const services = docSnap.data().services as Car_Service_Type[];
+
+      // find the service in the car
+
+      const foundService = services.find((item) => item.name === serviseName);
+
+      if (foundService) {
+        // delete the item
+        const updatedServices = services.map(({ name, items }) => {
+          if (name === serviseName) {
+            return {
+              name,
+              items: items.filter((item) => item.id !== itemId),
+            } as Car_Service_Type;
+          } else {
+            return { name, items };
+          }
+        });
+
+        await updateDoc(docRef, {
+          services: updatedServices,
+        });
+      } else {
+        console.log("No such car!");
+      }
+    } else {
+      console.log("No such car!");
+    }
+  } catch (error) {
+    console.log("Error while deleting item: ", error);
   }
 };
